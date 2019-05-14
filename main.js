bdf1ca0adc50ad3a914f34ac35ba5aa26023052c
Olya <bizarre444@users.noreply.github.com>
2019-05-14 20:21:50 +0300

    +comments

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var btn = document.getElementById('btn');
 // console.log(btn);
 
 btn.addEventListener('click', function(){
-    var x = +document.getElementById('first_num').value;
+    var x = +document.getElementById('first_num').value; //получаем первое введенное значение, по умолчанию 0.1
     // console.log(x);
-    var y = +document.getElementById('second_num').value;
+    var y = +document.getElementById('second_num').value; //получаем второе введенное значение, по умолчанию 0.2
     // console.log(y);
     result = x + y;
-    alert('Результат: ' + Math.round(result*10)/10);
+    alert('Результат: ' + Math.round(result*10)/10); //выводим результат, попутно округляя до необходимого количество знаков после запятой
 })
